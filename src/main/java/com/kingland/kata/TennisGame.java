f34064af796d293c9558326c521e2b8570d02ece
tphif <tim.phifer@kingland.com>
2020-12-18 01:17:03 +0000

    First commit

---
+++ src/main/java/com/kingland/kata/TennisGame.java
@@ -0,0 +1,6 @@
+package com.kingland.kata;
+
+public interface TennisGame {
+    void wonPoint(String playerName);
+    String getScore();
+}
---
+++ src/test/java/com/kingland/kata/TennisTest.java
@@ -0,0 +1,14 @@
+package com.kingland.kata;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class TennisTest {
+
+  @Test
+  void testSomeStuff() {
+    assertThat(40 + 2).isEqualTo(42);
+  }
+
+}
